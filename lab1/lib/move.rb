7498e2682fbaac6aab06195b0d971db189f32c32
Michael Ries <michael@riesd.com>
2009-05-03 03:07:51 -0600

    added lab1 code

---
+++ lab1/lib/move.rb
@@ -0,0 +1,28 @@
+module BraveZealot
+
+# The move class encapsulates the idea and data necessary
+# to change the speed and angle of a tank
+class Move
+  attr_accessor :speed, :angvel
+
+  def initialize(speed, angvel)
+    #set the speed between -1 and 1    
+    if ( speed > 1 ) then
+      @speed = 1;
+    else if (speed < -1 ) ten
+      @speed = -1;
+    else
+      @speed = speed;
+    end
+    
+    #set the angvel between -1 and 1
+    if ( angvel > 1 ) then
+      @angvel = 1
+    else if ( angvel < -1 ) then
+      @angvel = -1
+    else
+      @angvel = angvel;
+    end
+  end
+
+end
---
+++ lab1/lib/tank.rb
@@ -0,0 +1,5 @@
+module BraveZealot
+
+class Tank
+
+end
