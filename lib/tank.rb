a469301bb7317acaddb5b5e52dad31fbf44e2fd9
Duane Johnson <duane.johnson@gmail.com>
2009-05-08 09:18:52 -0600

    First attempt at dummy tank--too smart?

--- lib/tank.rb
+++ lib/tank.rb
@@ -1,10 +1,25 @@
 module BraveZealot
 
   class Tank
+    # tank :: BraveZealot::Communicator::Tank -> Data object
+    attr_accessor :tank
+    
     # Expects a Communicator::Tank struct for init
     def initialize(hq, tank)
       @hq, @tank = hq, tank
       start
+    end
+    
+    def refresh(freshness, &block)
+      @hq.refresh_mytanks(&block)
+    end
+    
+    def sleep(time, &block)
+      EventMachine::Timer.new(time, &block)
+    end
+    
+    def bind(*args, &block)
+      hq.bind(*args, &block)
     end
     
     def method_missing(m, *args, &block)
@@ -20,11 +35,29 @@ module BraveZealot
   
   class DummyTank < Tank
     def start
-      speed 1.0
-    end
-    
-    def timer(slice)
-      puts "slice: #{slice}"
+      speed(1.0)
+      bind(:hit) do
+        speed(-0.05)
+        start_angle = angle
+        turn60 = Proc.new do
+          refresh(0.2) do
+            angvel(1.0) do
+              sleep(0.1) do
+                if abs(start_angle - angle) < 60
+                  # Keep turning
+                  turn60.call
+                else
+                  # We've turned enough, now go straight
+                  angvel(0.0)
+                  speed(1.0)
+                end
+              end
+            end
+          end
+        end
+        
+        turn60.call
+      end
     end
   end
 
