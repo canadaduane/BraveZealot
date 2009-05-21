3b79f35edb8c16f2e64506cb5a96e2db93382cca
Duane Johnson <duane.johnson@gmail.com>
2009-05-21 12:30:06 -0600

    Removed Agent:: module. Added DummyStates module and SmartStates module. (this might be a nice way to separate out agent code that is mean to work as a unit).

--- lib/agent.rb
+++ lib/agent.rb
@@ -1,6 +1,59 @@
 bzrequire 'lib/communicator'
 
 module BraveZealot
+  module DummyStates
+    # Shortcut state for :forward_until_hit
+    def state_dummy
+      @state = :dummy_forward
+    end
+    
+    def state_dummy_forward
+      curr_speed = Math.sqrt(@tank.vx**2 + @tank.vy**2)
+      # Check if we've hit something
+      @state =
+        if curr_speed < 24.5
+          speed(-0.1)
+          :dummy_random_turn
+        else
+          :dummy_forward
+        end
+    end
+    
+    def state_dummy_random_turn
+      angvel(rand < 0.5 ? -1.0 : 1.0) do
+        sleep(2.0) do
+          speed 1.0
+          angvel(0.0)
+          @state = :dummy_accel_once
+        end
+      end
+    end
+    
+    def state_dummy_accel_once
+      @state = :wait
+      sleep(1.5) { @state = :dummy_forward }
+    end
+  end
+  
+  module SmartStates
+    def state_smart
+      if @goal
+        path = hq.map.search(@tank, @goal)
+        puts "Path: #{path.inspect}"
+        # move = @goal.suggest_move(@tank.x, @tank.y, angle)
+        # speed move.speed
+        # angvel move.angvel
+      else
+        @state = :smart_return_home
+      end
+    end
+    
+    def state_smart_return_home
+      @goal = hq.my_base.center
+      @state = :smart
+    end
+  end
+
   class Agent
     # hq   :: Headquarters  -> The headquarters object
     # tank :: Tank          -> Data object
@@ -9,6 +62,9 @@ module BraveZealot
     # state :: Symbol  -> :capture_flag, :home
     # goal :: Coord   -> Coordinate indicating where the agent is headed
     attr_accessor :state, :goal
+    
+    include DummyStates
+    include SmartStates
     
     # See above for definitions of hq and tank
     def initialize(hq, tank, initial_state = nil)
@@ -31,47 +87,6 @@ module BraveZealot
       # do nothing
     end
     
-    # Shortcut state for :forward_until_hit
-    def state_dummy
-      @state = :forward_until_hit
-    end
-    
-    def state_forward_until_hit
-      curr_speed = Math.sqrt(@tank.vx**2 + @tank.vy**2)
-      # Check if we've hit something
-      @state =
-        if curr_speed < 24.5
-          speed(-0.1)
-          :random_turn
-        else
-          :forward_until_hit
-        end
-    end
-    
-    def state_random_turn
-      angvel(1.0) do
-        sleep(2.0) do
-          speed 1.0
-          angvel(0.0)
-          @state = :accel_once
-        end
-      end
-    end
-    
-    def state_accel_once
-      @state = :wait
-      sleep(1.5) { @state = :forward_until_hit }
-    end
-    
-    def state_smart
-      if @goal
-        move = @goal.suggest_move(@tank.x, @tank.y, angle)
-        speed move.speed
-        angvel move.angvel
-      end
-      :smart
-    end
-    
     def refresh(freshness, &block)
       @hq.refresh(:mytanks, freshness, &block)
     end
@@ -90,4 +105,5 @@ module BraveZealot
       end
     end
   end
+  
 end
