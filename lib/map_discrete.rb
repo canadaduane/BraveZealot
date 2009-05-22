c7e52a3d125fba3d9f101a364b8746ba4a93a072
Duane Johnson <duane.johnson@gmail.com>
2009-05-22 09:52:12 -0600

    Added path visualization to pdf

--- lib/map_discrete.rb
+++ lib/map_discrete.rb
@@ -58,6 +58,28 @@ module BraveZealot
       @astar.search(s[0], s[1], g[0], g[1])
     end
     
+    def to_pdf(pdf = nil, options = {})
+      super do |pdf|
+        
+        if options[:paths]
+          pdf.stroke_style(PDF::Writer::StrokeStyle.new(2))
+          pdf.stroke_color Color::RGB::Red
+          # p options[:paths]
+          options[:paths].each do |path|
+            if path
+              x, y = array_to_world_coordinates(path[0][0], path[0][1])
+              shape = pdf.move_to(x, y)
+              path[1..-1].each do |ax, ay|
+                x, y = array_to_world_coordinates(ax, ay)
+                shape.line_to(x, y)
+              end
+              shape.stroke
+            end
+          end
+          pdf.stroke_style(PDF::Writer::StrokeStyle.new(1))
+        end
+      end
+    end
   end
 
 end
