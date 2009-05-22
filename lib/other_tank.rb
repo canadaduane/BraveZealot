116fc3b47bb9c5e56fd681c6517296b415e04269
Duane Johnson <duane.johnson@gmail.com>
2009-05-22 16:53:35 -0600

    Don't draw dead tanks

--- lib/other_tank.rb
+++ lib/other_tank.rb
@@ -3,6 +3,10 @@ module BraveZealot
     def to_coord
       Coord.new(x,y)
     end
+    
+    def alive?
+      status != 'dead'
+    end
 
     def to_pdf(pdf = nil, options = {})
       return if pdf.nil?
@@ -10,16 +14,18 @@ module BraveZealot
       # puts "Draw mytank #{x}, #{y}"
       fill, stroke = team_colors(self.color)
       
-      # Draw the tank
-      pdf.stroke_style(PDF::Writer::StrokeStyle.new(1))
-      pdf.stroke_color stroke
-      pdf.fill_color   fill
+      if alive?
+        # Draw the tank
+        pdf.stroke_style(PDF::Writer::StrokeStyle.new(1))
+        pdf.stroke_color stroke
+        pdf.fill_color   fill
       
-      pdf.circle_at(x, y, 10).fill_stroke
-      c = Math.cos(angle)
-      s = Math.sin(angle)
+        pdf.circle_at(x, y, 10).fill_stroke
+        c = Math.cos(angle)
+        s = Math.sin(angle)
       
-      pdf.line(x, y, x+c*10, y+s*10).stroke
+        pdf.line(x, y, x+c*10, y+s*10).stroke
+      end
       
     end
   end
