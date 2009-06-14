d945691c6fe80ad9c6972e3fd09fc043058cd414
Duane Johnson <duane.johnson@gmail.com>
2009-06-13 21:42:31 -0600

    Smart states undergoing big changes (changing to SeekStates)

--- lib/agent_states/smart.rb
+++ lib/agent_states/smart.rb
@@ -1,62 +1,95 @@
 module BraveZealot
-  module SmartStates
+  module SeekStates
     attr_accessor :path
     
-    def smart
-      # hq.periodic_action(2.5, 5) do
+    def seek
+      seek_enemy_flag if @goal.nil?
+      
+      hq.periodic_action(2.5, 3) do
         @path = hq.map.search(@tank, @goal)
-      # end
-      # transition(:smart, :smart_follow_path)
+        hq.map.randomize_path!(@path) if $options.random_path
+      end
       smart_follow_path
+      # transition(:smart, :smart_follow_path)
     end
     
-    def smart_follow_path
+    def seek_fast
       if @path.nil?
-        @state = :smart
+        @state = :seek
         return
       end
       
-      @waypoint ||= @path.first || @tank
+      if (@path = hq.map.search(@tank, @goal))
+        if path.size >= 2
+          move = smart_move_to(path[0].vector_to(path[1]))
       
-      # parts = @path[0..10].size
-      # avg = @path[0..10].inject(0.0){ |sum, coord| sum + @tank.vector_to(coord).length } / parts
-      # puts "Here: #{@tank.x}, #{@tank.y}"
-      # puts "Average dist from here: #{avg}"
-      
-      distance = @tank.vector_to(@waypoint).length
-      while !@path.empty? and distance < 30
-        @waypoint = @path.shift
-        distance = @tank.vector_to(@waypoint).length
-      end 
-      
-      group = PfGroup.new
-      group.add_field(Pf.new(@waypoint.x, @waypoint.y, hq.map.world_size, 5, 1))
-      move = group.suggest_move(@tank.x, @tank.y, @tank.angle)
-      
-      speed move.speed
-      angvel move.angvel
-      
-      transition(:smart_follow_path, :smart_look_for_enemy_flag) if @path.empty?
+          speed move.speed
+          angvel move.angvel
+        end
+        
+        transition(:smart_follow_path, :smart_look_for_enemy_flag) if @path.empty?
+      end
     end
     
-    def smart_look_for_enemy_flag
+    def seek_slow
+      parts = @path[0..10].size
+      avg = @path[0..10].inject(0.0){ |sum, coord| sum + @tank.vector_to(coord).length } / parts
+      puts "Here: #{@tank.x}, #{@tank.y}"
+      puts "Average dist from here: #{avg}"
+    
+      distance = @tank.vector_to(path.first).length
+      while !@path.empty? and distance < 30
+        distance = @tank.vector_to(@path.shift).length
+      end 
+    
+      group = PfGroup.new
+      group.add_field(Pf.new(@waypoint.x, @waypoint.y, hq.map.world_size, 5, 1))
+      move = group.suggest_fastest_move(@tank.x, @tank.y, @tank.angle)
+    end
+    
+    def seek_enemy_flag
       if hq.enemy_flag_exists?
-        puts "Enemy flags:"
-        p hq.enemy_flags
         @goal = hq.enemy_flags.randomly_pick(1).first
-        @state = :smart
+        puts "Seeking enemy flag: #{@goal.color} at (#{@goal.x}, #{@goal.y})"
+        transition(:seek_enemy_flag, :seek)
       else
         # Remain in :smart_look_for_enemy_flag state otherwise
       end
     end
     
-    def smart_return_home
-      puts "Going into the the smart_return_home state..."
+    def seek_home_base
       @goal = hq.my_base.center
-      puts "goal at #{@goal.inspect}, path=#{@path}, idx=#{@idx}, dest=#{@dest}, group=#{@group}"
-      push_next_state(:smart, :dummy)
-      @state = :smart
+      puts "Seeking home base: (#{@goal.x}, #{@goal.y})"
+      transition(:seek_home_base, :seek)
     end
+    
+    def angle_diff(x, y)
+      Math.atan2(Math.sin(x - y), Math.cos(x - y))
+    end
+      
+    def smart_vector_move(vector)
+      # river_current = coord.vector_to(next_coord)
+      # direction = @tank.vector_to(coord)
+      # 
+      # alpha = 0.0
+      # # alpha = 0.5
+      # # alpha = 30 / [30, direction.length].max
+      # hybrid = Vector.new(alpha * direction.x + (1 - alpha) * river_current.x,
+      #                     alpha * direction.y + (1 - alpha) * river_current.y)
+      # 
+      delta = angle_diff(vector.angle, @tank.angle)
+      # 
+      # puts "current | x: #{@tank.x}, y: #{@tank.y}, angle: #{@tank.angle}"
+      # puts "river_current angle: #{river_current.angle}"
+      # puts "direction angle: #{direction.angle}"
+      # puts "alpha: #{alpha}"
+      # puts "hybrid angle: #{hybrid.angle}"
+      # puts "delta angle: #{delta}"
+      # puts "distance: #{direction.length}"
+      
+      return Move.new(1.0, delta / (15 * $options.refresh))
+    end
+    
   end
   
 end
