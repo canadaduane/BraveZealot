e538a246afd894b77a873fe9ba3f3b467197c1e8
Michael Ries <michael@riesd.com>
2009-06-14 23:08:11 -0600

    fixed a few fatal errors that can crash the defenders

--- lib/agent_states/hunting.rb
+++ lib/agent_states/hunting.rb
@@ -346,16 +346,11 @@ module BraveZealot
 		def hunter_capture_flag
 			trace "hunter_capture_flag"
 
-			if @hq.we_have_enemy_flag?
-				goal_home_base
-				@state = :hunter_return_to_base
-				debug "hunter_capture_flag -> hunter_return_to_base"
-			else
-		    move = @group.suggest_move(@tank.x, @tank.y, @tank.angle)
-		    
-		    speed move.speed
-		    angvel move.angvel
-			end
+			push_next_state(:seek, :sniper_flag_captured)
+      f = hq.enemy_flags.first
+      puts "enemy tanks are dead - transitioning to smart search for flag at #{f.x}, #{f.y}"
+      @goal = Coord.new(f.x, f.y)
+      @state = :seek
     end
 
 		def goal_enemy_flag
@@ -496,7 +491,9 @@ module BraveZealot
 		end
 
 		def log_shot()
-			debug "shot fired at: #{@hunter_target.callsign}, #{@hunter_target.status}"
+      unless @hunter_target.nil? then
+        debug "shot fired at: #{@hunter_target.callsign}, #{@hunter_target.status}"
+      end
 		end
 
 		def log_cancelled_shot()
